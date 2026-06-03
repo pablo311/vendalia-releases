74fc81e2edd00fa9cefe557dfeb4e2e90513f12e
pablo311 <pablo_zarza02@hotmail.com>
2026-06-03 15:11:12 -0300

    auto: mobile/lib/supabase.ts  [15:11]

--- mobile/lib/supabase.ts
+++ mobile/lib/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? 'https://iuwximhtkztnuscpfutn.supabase.co'
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1d3hpbWh0a3p0bnVzY3BmdXRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxODM2MzksImV4cCI6MjA5NTc1OTYzOX0.gbCjJrSxGC0l6jtVkbyIiL_NhHg0RIDVzYjAq22V0_Q'
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
